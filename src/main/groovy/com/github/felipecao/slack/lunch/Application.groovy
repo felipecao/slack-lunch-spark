0ff56594d32944f141c9c2beea3488df04783ea1
Felipe Carvalho <felipe.carvalho@gmail.com>
2017-04-11 14:10:07 +1000

    trying to figure out what's wrong on Heroku

--- src/main/groovy/com/github/felipecao/slack/lunch/Application.groovy
+++ src/main/groovy/com/github/felipecao/slack/lunch/Application.groovy
@@ -12,12 +12,23 @@ class Application {
     private Logger log = LoggerFactory.getLogger(Application.class);
 
     Application(MongoStore mongoStore) {
+        port(getHerokuAssignedPort());
         before("/*", { q, a -> log.info("Received api call") } );
         get("/", {req, res -> "Slack-Lunch is working with Spark!"});
         path("/places", {
             post("/menu", new MenuCommand(mongoStore: mongoStore), new JsonTransformer());
             post("/show", new ShowCommand(mongoStore: mongoStore), new JsonTransformer());
         });
+    }
+
+    private static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 
     static void main(String[] args) {
